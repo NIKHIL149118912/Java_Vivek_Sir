420f1d83f85e6cd05307bbd4feacae3f5f259c6a
NIKHIL TOMAR <nikhiltomar1491@gmail.com>
2025-09-13 09:20:27 +0530

    7 More Patterns added

--- Java/Pattern Programs/Pattern.java
+++ Java/Pattern Programs/Pattern.java
@@ -16,6 +16,28 @@ public class Pattern {
             System.out.println();
         }
     }
+    void openMouth(int n)
+    {
+        int i,j;
+        for(i=0;i<n;i++)
+        {
+            for(j=n;j>i;j--) // OR j=0;j<n-i+1; j++
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(i=2;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+        }
+        
+    }
     
     void printHalfPyramid(int n) {
         int i, j;
@@ -175,6 +197,88 @@ public class Pattern {
             System.out.println();
         }
     }
+
+    void hollowRombus(int n)
+    {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (j == 1 || j == n || i == 1 || i == n) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    void hollowTriangle(int n)
+    {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    void upRightPyramid(int n)
+    {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("  * ");
+            }
+            System.out.println();
+        }
+    }
+    void reversePyramid(int n)
+    {
+         for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("  * ");
+            }
+            System.out.println();
+        }
+    }
+
+    void Rhombus(int n)
+    {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 1; j <= n; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    void equilateralTriangle(int n)
+    {
+        for (int i = 1; i <= n; i++) {
+            // Inner loop for leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // Inner loop for printing stars
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         int choice;
     do{
@@ -183,29 +287,43 @@ public class Pattern {
         int line= sc.nextInt();
         System.out.println();
         Pattern s = new Pattern();
-            s.printStar1(line);
+            s.openMouth(line); //1
         System.out.println();
-            s.printHalfPyramid(line);
+            s.printStar1(line);//2
         System.out.println();
-            s.printStar2(line);
+            s.printHalfPyramid(line);//3
         System.out.println();
-            s.printHalfPyramidNumbers(line);
+            s.printStar2(line);//4
         System.out.println();
-            s.printCharPattern(line);
+            s.printHalfPyramidNumbers(line);//5
         System.out.println();
-            s.printHollowRect(line);
+            s.printCharPattern(line);//6
         System.out.println();
-            s.invRotHalfPyr(line);
+            s.printHollowRect(line);//7
         System.out.println();
-            s.inverted_HalfPyramid_with_Decreasing_numbers(line);
+            s.reversePyramid(line);//8
         System.out.println();
-            s.inverted_HalfPyramid_with_increasing_numbers(line);
+            s.equilateralTriangle(line);//9
         System.out.println();
-            s.floydsTriangle(line);
+            s.invRotHalfPyr(line);//10
         System.out.println();
-            s.Alternate_01_triangle(line);
+            s.Rhombus(line);//11
         System.out.println();
-            s.merged_triangle(line);
+            s.inverted_HalfPyramid_with_Decreasing_numbers(line);//12
+        System.out.println();
+            s.inverted_HalfPyramid_with_increasing_numbers(line);//13
+        System.out.println();
+            s.floydsTriangle(line);//14
+        System.out.println();
+            s.Alternate_01_triangle(line);//15
+        System.out.println();
+            s.merged_triangle(line);//16
+        System.out.println();
+            s.hollowRombus(line);//17
+        System.out.println();
+            s.hollowTriangle(line);//18
+        System.out.println();
+            s.upRightPyramid(line);//19
         System.out.println("Do you Wish to continue ? press 1 for YES & 0 for NO");
         choice = sc.nextInt();
         sc.close();
